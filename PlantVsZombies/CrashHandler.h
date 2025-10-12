a3b2b21bfdcbcb4ac422704341d8bd6010776f06
rgthddei67 <Hanzj2011@outlook.com>
2025-10-12 14:46:46 +0800

    update CrashHandler

--- PlantVsZombies/CrashHandler.h
+++ PlantVsZombies/CrashHandler.h
@@ -1,30 +1,50 @@
 #pragma once
 #ifndef _CRASH_HANDLER_H
 #define _CRASH_HANDLER_H
-#define NOMINMAX
+
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#define NOMINMAX
+#include <windows.h>
 #include <string>
-#include <sstream>
-#include <DbgHelp.h>
-#include <shellapi.h>
 
 class CrashHandler {
 private:
-    static LPTOP_LEVEL_EXCEPTION_FILTER previousFilter;
+    static PVOID vehHandle;
+    static LONG64 lastUACTime;
 
 public:
+    // 初始化崩溃处理器
     static void Initialize();
+
+    // 清理崩溃处理器
     static void Cleanup();
 
 private:
-    static LONG WINAPI UnhandledExceptionFilter(PEXCEPTION_POINTERS exceptionInfo);
+    // VEH 异常处理函数
+    static LONG WINAPI VectoredExceptionHandler(PEXCEPTION_POINTERS exceptionInfo);
+
+    // 处理崩溃的主函数
+    static void HandleCrash(PEXCEPTION_POINTERS exceptionInfo);
+
+    // 是不是崩溃异常
+    static bool IsCrashException(DWORD exceptionCode);
+
+    // 生成崩溃报告
+    static std::string GenerateCrashReport(PEXCEPTION_POINTERS exceptionInfo);
+
+    // 显示崩溃对话框
+    static void ShowCrashDialog(PEXCEPTION_POINTERS exceptionInfo, const std::string& reportPath);
+
+    // 获取异常代码的描述
     static std::string GetExceptionCodeString(DWORD exceptionCode);
-    static std::string GetModuleName(HMODULE module);
-    static void GenerateCrashReport(PEXCEPTION_POINTERS exceptionInfo, const std::string& reportPath);
-    static void ShowCrashDialog(const std::string& errorMessage, const std::string& reportPath);
+
+    // 获取栈跟踪信息
     static std::string GetStackTrace(PEXCEPTION_POINTERS exceptionInfo);
-    static void GenerateMiniDump(PEXCEPTION_POINTERS exceptionInfo, const std::string& dumpPath);
+
+    // 获取模块名称
+    static std::string GetModuleName(HMODULE module);
+
+    static bool IsUACByTimeWindow(PEXCEPTION_POINTERS exceptionInfo);
 };
 
 #endif
